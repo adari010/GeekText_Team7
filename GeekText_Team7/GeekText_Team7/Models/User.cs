900e8c1754479aaefd4aee4cb760fc1134664eca
Shafeeque Khan <shafeequeable@gmail.com>
2017-06-26 14:00:30 -0400

    Added an Account Controller to manage account info. Added appropriate account view models to handle the views for the account. Added services. Added more views.

--- GeekText_Team7/GeekText_Team7/Models/User.cs
+++ GeekText_Team7/GeekText_Team7/Models/User.cs
@@ -1,18 +1,19 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace GeekText_Team7.Models
 {
-    public class User
+    public class User : IdentityUser 
     {
-        public int Id { get; set; }
+        public int ID { get; set; }
         public DateTime DOB { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Sex { get; set; }
-        public string Email { get; set; }
+        public string email { get; set; }
         public string Alias { get; set; }
         public string Street { get; set; }
         public string City { get; set; }
