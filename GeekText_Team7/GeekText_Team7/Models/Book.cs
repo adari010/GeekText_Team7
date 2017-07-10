e6ca0df93bb1f376f0433b35f207f5cb47fe3eb6
Michael Quiros <mquir016@fiu.edu>
2017-07-10 17:11:17 -0400

    one more revert

---
+++ GeekText_Team7/GeekText_Team7/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GeekText_Team7.Models
+{
+    public partial class Book
+    {
+        public Book()
+        {
+            BookAuthor = new HashSet<BookAuthor>();
+        }
+
+        public int Id { get; set; }
+        public string Isbn { get; set; }
+        public string Title { get; set; }
+        public int UserId { get; set; }
+        public string Description { get; set; }
+        public string Genre { get; set; }
+
+        public virtual ICollection<BookAuthor> BookAuthor { get; set; }
+        public virtual User User { get; set; }
+    }
+}
---
+++ GeekText_Team7/GeekText_Team7/Models/BookAuthor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GeekText_Team7.Models
+{
+    public partial class BookAuthor
+    {
+        public int BookId { get; set; }
+        public int AuthorId { get; set; }
+        public byte Order { get; set; }
+
+        public virtual Author Author { get; set; }
+        public virtual Book Book { get; set; }
+    }
+}
