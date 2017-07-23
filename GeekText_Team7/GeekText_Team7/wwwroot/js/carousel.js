0f6b349e5ba770fae7f969c1212b53f4a323ce3f
Michael Quiros <mquir016@fiu.edu>
2017-07-23 11:39:45 -0400

    Added files for multi item carousel

--- GeekText_Team7/GeekText_Team7/wwwroot/js/carousel.js
+++ GeekText_Team7/GeekText_Team7/wwwroot/js/carousel.js
@@ -1,11 +1,4 @@
-﻿// Instantiate the Bootstrap carousel
-$('.multi-item-carousel').carousel({
-    interval: false
-});
-
-// for every slide in carousel, copy the next slide's item in the slide.
-// Do the same for the next, next item.
-$('.multi-item-carousel .item').each(function () {
+﻿$('.carousel slide .item').each(function () {
     var next = $(this).next();
     if (!next.length) {
         next = $(this).siblings(':first');
@@ -14,7 +7,8 @@ $('.multi-item-carousel .item').each(function () {
 
     if (next.next().length > 0) {
         next.next().children(':first-child').clone().appendTo($(this));
-    } else {
+    }
+    else {
         $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
     }
-});s
+});
