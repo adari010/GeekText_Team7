249d2610b9561754a34e8e9d6dc484b32e9fcca3
rroda001 <rroda001@fiu.edu>
2017-07-24 01:13:53 -0400

    Updated Insert.php

--- GeekText_Team7/GeekText_Team7/Views/Manage/insert.php
+++ GeekText_Team7/GeekText_Team7/Views/Manage/insert.php
@@ -9,6 +9,9 @@ $firstname = $_POST['firstname'];
 $lastname = $_POST['lastname'];
 $ccnumber = $_POST['ccnumber'];
 $billaddr = $_POST['billaddr'];
+$city = $_POST['city'];
+$state = $_POST['state'];
+$zipcode = $_POST['zipcode'];
 $cvv = $_POST['cvv'];
 $expmonth = $_POST['expmonth'];
 $expyear = $_POST['expyear'];
@@ -17,16 +20,14 @@ $firstname = mysqli_real_escape_string($connection, $firstname);
 $lastname = mysqli_real_escape_string($connection, $lastname);
 $ccnumber = mysqli_real_escape_string($connection, $ccnumber);
 $billaddr = mysqli_real_escape_string($connection, $billaddr);
+$city = mysqli_real_escape_string($connection, $city);
+$state = mysqli_real_escape_string($connection, $state);
+$zipcode = mysqli_real_escape_string($connection, $zipcode);
 $cvv = mysqli_real_escape_string($connection, $cvv);
 $expmonth = mysqli_real_escape_string($connection, $expmonth);
 $expyear = mysqli_real_escape_string($connection, $expyear);
 
-$query = "INSERT INTO [dbo.CreditCard] (FirstName, LastName, CCNumber, BillAddr, CVV, ExpMonth, ExpYear) VALUES ('"$firstname"','"$lastname"','"$ccnumber"','"$billaddr"','"$cvv"','"$expmonth"','"$expyear"' )";
+$query = "INSERT INTO [dbo.CreditCard] (FirstName, LastName, CCNumber, BillAddr, City, State, ZipCode, CVV, ExpMonth, ExpYear) VALUES ('"$firstname"','"$lastname"','"$ccnumber"','"$billaddr"', '"$city"','"$state"','"$zipcode"','"$cvv"','"$expmonth"','"$expyear"')";
 
 $result = mysqli_query($connection, $query);
-
-if ($result) {
-header('Location: insert.php');
-        } else {
-            die("Database query failed. " . mysqli_error($connection)); 
 ?>
