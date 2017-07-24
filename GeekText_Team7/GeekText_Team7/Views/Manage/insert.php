445e816a4a4cfb179bdda13c7780a1a6bd6e59c3
rroda001 <rroda001@fiu.edu>
2017-07-24 01:33:54 -0400

    Optimized store procedure

--- GeekText_Team7/GeekText_Team7/Views/Manage/insert.php
+++ GeekText_Team7/GeekText_Team7/Views/Manage/insert.php
@@ -1,33 +1,19 @@
 <?php
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "password";
-$dbname = "[dbo.CreditCard]";
-$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+$data = json_decode(file_get_contents("php://input"));
 
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-$ccnumber = $_POST['ccnumber'];
-$billaddr = $_POST['billaddr'];
-$city = $_POST['city'];
-$state = $_POST['state'];
-$zipcode = $_POST['zipcode'];
-$cvv = $_POST['cvv'];
-$expmonth = $_POST['expmonth'];
-$expyear = $_POST['expyear'];
+$firstname = mysql_real_escape_string($data->$firstname);
+$lastname = mysql_real_escape_string($data->$lastname);
+$ccnumber = mysql_real_escape_string($data->$ccnumber);
+$billaddr = mysql_real_escape_string($data->$billaddr);
+$city = mysql_real_escape_string($data->$city);
+$state = mysql_real_escape_string($data->$state);
+$zipcode = mysql_real_escape_string($data->$zipcode);
+$cvv = mysql_real_escape_string($data->$cvv);
+$expmonth = mysql_real_escape_string($data->$expmonth);
+$expyear = mysql_real_escape_string($data->$expyear);
 
-$firstname = mysqli_real_escape_string($connection, $firstname);
-$lastname = mysqli_real_escape_string($connection, $lastname);
-$ccnumber = mysqli_real_escape_string($connection, $ccnumber);
-$billaddr = mysqli_real_escape_string($connection, $billaddr);
-$city = mysqli_real_escape_string($connection, $city);
-$state = mysqli_real_escape_string($connection, $state);
-$zipcode = mysqli_real_escape_string($connection, $zipcode);
-$cvv = mysqli_real_escape_string($connection, $cvv);
-$expmonth = mysqli_real_escape_string($connection, $expmonth);
-$expyear = mysqli_real_escape_string($connection, $expyear);
+mysql_connect("localhost", "root", ""); 
+mysql_select_db("CreditCard");
 
-$query = "INSERT INTO [dbo.CreditCard] (FirstName, LastName, CCNumber, BillAddr, City, State, ZipCode, CVV, ExpMonth, ExpYear) VALUES ('"$firstname"','"$lastname"','"$ccnumber"','"$billaddr"', '"$city"','"$state"','"$zipcode"','"$cvv"','"$expmonth"','"$expyear"')";
-
-$result = mysqli_query($connection, $query);
+mysqli_query("INSERT INTO CreditCard (FirstName, LastName, CCNumber, BillAddr, City, State, ZipCode, CVV, ExpMonth, ExpYear) VALUES ('"$firstname"','"$lastname"','"$ccnumber"','"$billaddr"', '"$city"','"$state"','"$zipcode"','"$cvv"','"$expmonth"','"$expyear"')");
 ?>
