a94907893fa8b80142b88952aa206785d8b3cbf3
Shafeeque Khan <skhan064@fiu.edu>
2017-07-25 14:22:37 -0400

    Added cookie implementation to shopping cart.

--- GeekText_Team7/GeekText_Team7/wwwroot/js/site.js
+++ GeekText_Team7/GeekText_Team7/wwwroot/js/site.js
@@ -1,5 +1,4 @@
-﻿// Write your Javascript code.
-//SHOPPING  CART
+﻿//SHOPPING  CART
 var days = 60;
 var now = new Date();
 var time = now.getTime();
@@ -9,13 +8,91 @@ var cname = 'GeekTeam7_books';
 var cart = 0;
 var payTotal = 0;
 
-var cookie = GetCookie();
+var cookie;
 var output = '';
 
+initialize();
+
+function initialize() {
+    cookie = GetCookie();
+    checkout();
+}
+
+
+function checkout() {
+
+    var cartItems = $('#cartContent');
+
+    if (cartItems.length != 0) {
+
+        if (cookie != '') {
+
+
+            cookie = cookie.split(',');
+            if (cookie.length > 0) {
+
+                totalPrice = $('#price');
+
+                addItems = '';
+                //addItems = '<input type="hidden" name="item_name_1" value="Item Name 1">' +
+                //    '<input type="hidden" name="amount_1" value="1.00">' +
+                //    '<input type="hidden" name="item_name_2" value="Item Name 2">' +
+                //    '<input type="hidden" name="amount_2" value="2.00">' +
+                //    '<input type="submit" value="PayPal">';
+
+                //cookie = cookie.split(',');
+
+                payTotal = 0;
+
+                for (i = 0; i < cookie.length; i++) {
+                    book = cookie[i].split('|');
+                    price = parseFloat(book[2]);
+                    payTotal += price;
+
+                    addItems += '<input type="text" readonly="true" name="item_name_' + (i + 1) + '" value="' + book[0] + ' - $' + price + '">' +
+                        '<input type="hidden" name="amount_' + (i + 1) + '" value="' + price + '"><br/>';
+
+                    //$('#cartItems').prepend('<li><a href="#" class="removeItem" onclick="DeleteCookie( $(this) );" > (X)</a> - <a href="#">' + book[0].trim() + ' - ' + book[2].trim() + ' - $<span>' + book[1].trim() + '</a></li>');
+                    //value += (value == '') ? book[0].trim() + '|' + book[2].trim() + '|' + book[1].trim() : ',' + book[0] + '|' + book[2] + '|' + book[1];
+                }
+
+                cartItems.append(addItems + '<br/><span>Total: $' + payTotal.toFixed(2) + '</span><br/><input type="submit" value="Pay">');
+
+            }
+        }
+        else {
+            cartItems.text('No items in Shopping Cart');
+
+        }
+    }
+}
+
 function GetCookie() {
-    var value = '; ' + document.cookie();
+    var value = '; ' + decodeURI(document.cookie);
     var items = value.split('; ' + cname + '=');
-    cart = items.length();
+    //cart = items.length;
+    if (items.length > 1) {
+
+        items = items[1].split('%2C');
+
+        payTotal = 0;
+        value = '';
+
+        for (i = 1; i < items.length; i++) {
+            book = items[i].split('|');
+            payTotal += parseFloat(book[1]);
+
+            $('#cartItems').prepend('<li><a href="#" class="removeItem" onclick="DeleteCookie( $(this) );" > (X)</a> - <a href="#">' + book[0].trim() + ' - ' + book[2].trim() + ' - $<span>' + book[1].trim() + '</a></li>');
+            value += (value == '') ? book[0].trim() + '|' + book[2].trim() + '|' + book[1].trim() : ',' + book[0] + '|' + book[2] + '|' + book[1];
+        }
+        cart = i - 1;
+        $('#cartAmount').text(cart);
+        $('#payTotal').text(payTotal.toFixed(2));
+
+        return value;
+    }
+
+    return '';
 }
 
 
@@ -27,7 +104,7 @@ function DisplayCoookie() {
 
 function SetCookie(obj) {
 
-    isbn = obj.parent().next().text();
+    isbn = obj.parent().next().text().replace(' ', '').replace(' ', '').replace('\n', '').replace('\n', '');
     console.log(isbn);
     cart++;
     bookName = obj.parent().find('p').text();
@@ -37,6 +114,12 @@ function SetCookie(obj) {
     $('#cartItems').prepend('<li><a href="#" class="removeItem" onclick="DeleteCookie( $(this) );" > (X)</a> - <a href="#">' + bookName + ' - ' + isbn + ' - $<span>' + bookPrice + '</a></li>');
     $('#cartAmount').text(cart);
     $('#payTotal').text(payTotal);
+
+    if (typeof cookie != 'undefined')
+        cookie += ',' + bookName.trim() + '|' + bookPrice + '|' + isbn.trim();
+    else
+        cookie = bookName.trim() + '|' + bookPrice + '|' + isbn.trim();
+
 
     //alert( obj.nextSibling().text() );
     document.cookie = cname + '=' + encodeURIComponent(cookie) + '; expires=' + now.toGMTString() + '; path=/'
@@ -50,7 +133,11 @@ function SetCookie(obj) {
 
 function DeleteCookie(obj) {
 
-    bookPrice = obj.parent().find('span').text();
+    bookName = obj.parent().text().replace(' - ', ' ');
+    isbn = bookName;
+    isbn = isbn.substring((isbn.indexOf('-') + 1), isbn.replace('-', ' ').indexOf('-')).trim();
+    bookName = bookName.substring(5, bookName.indexOf('-') - 1).trim();
+    bookPrice = obj.parent().find('span').text().trim();
     payTotal = parseFloat($('#payTotal').text());
     payTotal -= bookPrice;
     $('#payTotal').text(payTotal.toFixed(2));
@@ -62,6 +149,12 @@ function DeleteCookie(obj) {
         $('#cartItems').html('');
 
     }
+
+    cookie = cookie.replace(',' + bookName + '|' + isbn + '|' + bookPrice, '');
+    cookie = cookie.replace(bookName + '|' + isbn + '|' + bookPrice, '');
+
+    document.cookie = cname + '=' + encodeURIComponent(cookie) + '; expires=' + now.toGMTString() + '; path=/'
+
 
 }
 
@@ -78,7 +171,6 @@ $('.removeItem').click(function (e) {
 });
 
 
-
 $(document).ready(function () {
 
 
@@ -90,8 +182,11 @@ $(document).ready(function () {
     //});
 
 
-
 });
+
+
+
+
 
 
 
